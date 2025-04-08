f8a089bbab8ba2a4d7b4dad40f86169a2f4aec05
Atuan0510 <phamtuan05102003@gmail.com>
2025-04-09 00:26:28 +0700

    Đưa dự án W3Schools lên GitHub

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52eb8c9cc8e248f1b38bddb8a11b8be4244f896")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c6560645a13fc2d0363615ffec61c86e7ba74c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
